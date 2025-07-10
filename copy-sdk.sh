12eb826c094006dc7c089d5c5284b0114c23a815
JakubMrozek <jakub.mrozek@gmail.com>
2025-07-10 15:29:32 +0200

    copy sdk, remove patch

---
+++ copy-sdk.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp /Users/JakubMrozekBrain/Projects/ads/packages/sdk/dist/* /Users/JakubMrozekBrain/Projects/nexus-native/node_modules/@kontextso/sdk/dist
+cp /Users/JakubMrozekBrain/Projects/ads/packages/sdk-react-native/dist/* /Users/JakubMrozekBrain/Projects/nexus-native/node_modules/@kontextso/sdk-react-native/dist
