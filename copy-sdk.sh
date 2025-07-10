f3e05736a5613a4aa5605f6ae7f46df5a1eb949d
JakubMrozek <jakub.mrozek@gmail.com>
2025-07-10 16:34:39 +0200

    add copy script

--- copy-sdk.sh
+++ copy-sdk.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
 
-cp /Users/JakubMrozekBrain/Projects/ads/packages/sdk/dist/* /Users/JakubMrozekBrain/Projects/nexus-native/node_modules/@kontextso/sdk/dist
-cp /Users/JakubMrozekBrain/Projects/ads/packages/sdk-react-native/dist/* /Users/JakubMrozekBrain/Projects/nexus-native/node_modules/@kontextso/sdk-react-native/dist
+ROOT_DIR=$(dirname "$(dirname "$(realpath "$0")")")
+
+PROJECT_ADS_DIR="$ROOT_DIR/ads"
+PROJECT_NEXUS_NATIVE_DIR="$ROOT_DIR/nexus-native"
+
+SDK_REACT_DIR="$PROJECT_ADS_DIR/packages/sdk"
+SDK_REACT_NATIVE_DIR="$PROJECT_ADS_DIR/packages/sdk-react-native"
+
+echo "Building SDK React"
+
+# build sdk react dir 
+cd "$SDK_REACT_DIR"
+npm run build:js
+
+echo ""
+echo "Building SDK React Native"
+
+# build sdk react native dir
+cd "$SDK_REACT_NATIVE_DIR"
+npm run build:js
+
+echo ""
+echo "Copying SDK React to Nexus Native"
+
+# cp "$SDK_REACT_DIR"/dist/* "$PROJECT_NEXUS_NATIVE_DIR/node_modules/@kontextso/sdk/dist"
+cp "$SDK_REACT_NATIVE_DIR"/dist/* "$PROJECT_NEXUS_NATIVE_DIR/node_modules/@kontextso/sdk-react-native/dist"
+
+echo "Done."
